7646901f0299d658875293c3d93e9a16e0282c49
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-12-26 02:33:51 +0300

    tokenId api

---
+++ client/pages/api/[tokenId].js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+  const image_url = "https://github.com/erenyusufduran/nft-collection/tree/master/client/public/cryptodevs";
+
+  if (tokenId >= 20) {
+    res.status(404).json({ error: "NOT FOUND" });
+  }
+
+  res.status(200).json({
+    name: "Crypto Dev #",
+    tokenId,
+    description: "Crypto Dev is a collection of developers in crypto.",
+    image: image_url + tokenId + ".svg",
+  });
+}
